0c0aa2c0e8c4faf29b64b04cfcd031f5e1c7178e
cluba <alguz@protonmail.com>
2020-03-11 17:28:51 +0900

    first commit

---
+++ StreetFlow/StreetFlow/Classes/Model/AppDefine.swift
@@ -0,0 +1,13 @@
+//
+//  AppDefine.swift
+//  StreetFlow
+//
+//  Created by Alex on 3/11/20.
+//  Copyright © 2020 ClubA. All rights reserved.
+//
+
+import UIKit
+
+class AppDefine: NSObject {
+
+}
---
+++ StreetFlow/StreetFlow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreetFlow
+//
+//  Created by Alex on 3/11/20.
+//  Copyright © 2020 ClubA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
