d2b485adff6ea22485a80c3cfce0e9a58eba695e
cluba <alexkrisko5563@gmail.com>
2020-03-12 19:05:29 +0900

    update storyboard

---
+++ StreetFlow/StreetFlow/Classes/VC/DealVC.swift
@@ -0,0 +1,30 @@
+//
+//  DealVC.swift
+//  StreetFlow
+//
+//  Created by Alex on 3/12/20.
+//  Copyright © 2020 ClubA. All rights reserved.
+//
+
+import UIKit
+
+class DealVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
