e7ce26538475f8c5357e03415fca7f8ca09745e6
cluba <alexkrisko5563@gmail.com>
2020-03-13 16:39:55 +0900

    update storyboard

---
+++ StreetFlow/StreetFlow/Classes/Model/ListCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListCell.swift
+//  StreetFlow
+//
+//  Created by Alex on 3/13/20.
+//  Copyright © 2020 ClubA. All rights reserved.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
