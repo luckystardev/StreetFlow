b8aee53cf158cb2fd7e2c0d20ddd22ed9bf76c1b
tmaas <alexkrisko5563@gmail.com>
2020-04-18 11:55:20 +0200

    disable dark mode, update menuVC

--- StreetFlow/StreetFlow/Classes/Delegate/AppDelegate.swift
+++ StreetFlow/StreetFlow/Classes/Delegate/AppDelegate.swift
@@ -19,6 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        if #available(iOS 13.0, *) {
+            window?.overrideUserInterfaceStyle = .light
+        }
+        
         locationManager.requestAlwaysAuthorization()
         
         let configuration = ParseClientConfiguration {
